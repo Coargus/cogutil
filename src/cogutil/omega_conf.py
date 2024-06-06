ab254d3d96781ae853cf7646db6ff529fee8a948
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-06-06 17:58:38 -0500

    update some util funcs

--- src/cogutil/omega_conf.py
+++ src/cogutil/omega_conf.py
@@ -27,7 +27,7 @@ def load_config_from_dict(
     return config
 
 
-def save_config_to_yaml(config: DictConfig, config_path: str) -> None:
+def save_config(config: DictConfig, config_path: str) -> None:
     """Save a DictConfig object to a yaml file."""
     with Path(config_path) as fp:
         OmegaConf.save(config=config, f=fp)
